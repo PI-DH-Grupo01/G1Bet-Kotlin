3c14a7fc74b2ed9500a2cbc4e98c1fe2e4952af0
Gabriel Marcolino <gabrielmr356@hotmail.com>
2022-11-07 22:46:58 -0300

    Criação do projeto

---
+++ src/test/java/br/com/g1bet/G1BetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.g1bet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G1BetApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
