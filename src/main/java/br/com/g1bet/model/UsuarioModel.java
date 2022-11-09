fd7905d5d46481300f902182da230989d551d9ea
Gabriel Marcolino <gabrielmr356@hotmail.com>
2022-11-08 23:20:09 -0300

    Adicionando classe usuário

---
+++ src/main/java/br/com/g1bet/model/UsuarioModel.java
@@ -0,0 +1,84 @@
+package br.com.g1bet.model;
+
+import java.time.LocalDate;
+
+public class UsuarioModel {
+
+    private Long id;
+    private String nome;
+    private Long cpf;
+    private LocalDate dataDeNascimento;
+    private String email;
+    private String senha;
+    private String chavePix;
+    private Double saldoUsuario;
+
+    public UsuarioModel() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(LocalDate dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getChavePix() {
+        return chavePix;
+    }
+
+    public void setChavePix(String chavePix) {
+        this.chavePix = chavePix;
+    }
+
+    public Double getSaldoUsuario() {
+        return saldoUsuario;
+    }
+
+    public void setSaldoUsuario(Double saldoUsuario) {
+        this.saldoUsuario = saldoUsuario;
+    }
+    
+}
