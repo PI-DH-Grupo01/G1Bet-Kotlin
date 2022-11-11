9b49f40158432a8f93e55c394c5fdbfa088ddc4e
Julio <juliobatistabraga@gmail.com>
2022-11-11 19:13:16 -0300

    CRUD da classe TimeModel

--- src/main/java/br/com/g1bet/model/TimeModel.java
+++ src/main/java/br/com/g1bet/model/TimeModel.java
@@ -1,12 +1,18 @@
 package br.com.g1bet.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "times")
@@ -19,6 +25,10 @@ public class TimeModel {
 	@NotNull
 	@Size(min = 5, max = 100)
 	private String nome;
+	
+	@ManyToMany(mappedBy = "time", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("time")
+	private List<PartidaModel> partidaModel;
 
 	public Long getId() {
 		return id;
