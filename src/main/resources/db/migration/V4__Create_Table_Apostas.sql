b443a2fe9e3a676a9980791df0474f594f35135c
Gabriel Marcolino <gabrielmr356@hotmail.com>
2022-11-15 10:43:42 -0300

    Atualizando as migrations

--- src/main/resources/db/migration/V4__Create_Table_Apostas.sql
+++ src/main/resources/db/migration/V4__Create_Table_Apostas.sql
@@ -1,10 +1,10 @@
-CREATE TABLE apostas
+CREATE TABLE aposta
 (
-    id_aposta      INT PRIMARY KEY AUTO_INCREMENT not null,
+    id             INT PRIMARY KEY AUTO_INCREMENT not null,
     id_usuario     BIGINT                         not null,
     id_partida     INT                            not null,
     tipo_de_aposta VARCHAR(20),
     valor_apostado FLOAT,
-    FOREIGN KEY (id_usuario) REFERENCES `usuarios` (id),
-    FOREIGN KEY (id_partida) REFERENCES `partidas` (id_partida)
+    FOREIGN KEY (id_usuario) REFERENCES `usuario` (id),
+    FOREIGN KEY (id_partida) REFERENCES `partida` (id)
 );
