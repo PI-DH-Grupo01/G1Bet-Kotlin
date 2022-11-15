7ee6c21e756d702c6ba10541185c0f213935ab8e
Amanda Ilg <amanda.ilg04@gmail.com>
2022-11-15 14:57:32 -0300

    Modificação da Controller de Aposta e criação de seu service e de uma parte da segurança do Usuário

--- src/main/java/br/com/g1bet/repository/ApostaRepository.java
+++ src/main/java/br/com/g1bet/repository/ApostaRepository.java
@@ -1,6 +1,7 @@
 package br.com.g1bet.repository;
 
 import br.com.g1bet.model.Aposta;
+import net.bytebuddy.jar.asm.commons.Remapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,5 @@ import java.util.List;
 @Repository
 public interface ApostaRepository extends JpaRepository<Aposta, Long> {
     List<Aposta> findAllByTipoContainingIgnoreCase(String tipo);
+
 }
