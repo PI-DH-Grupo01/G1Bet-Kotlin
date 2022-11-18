a6c7e854dd4c4f34bb421241a84f531404ab0be4
Gabriel Marcolino <gabrielmr356@hotmail.com>
2022-11-17 23:22:51 -0300

    Atualizando tabelas e adicionando times e partidas

---
+++ src/main/resources/db/migration/V7__Insert_Values_Paridas.sql
@@ -0,0 +1,102 @@
+USE g1bet;
+
+-- TIME 1 CASA
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (1, 2, NULL, '2023-01-01 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (1, 3, NULL, '2023-01-05 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (1, 4, NULL, '2023-01-08 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (1, 5, NULL, '2023-01-10 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (1, 6, NULL, '2023-01-15 21:00:00');
+
+-- TIME 2 CASA
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (2, 1, NULL, '2023-01-20 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (2, 3, NULL, '2023-01-25 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (2, 4, NULL, '2023-01-30 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (2, 5, NULL, '2023-02-01 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (2, 6, NULL, '2023-02-05 21:00:00');
+
+-- TIME 3 CASA
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (3, 1, NULL, '2023-02-10 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (3, 2, NULL, '2023-02-15 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (3, 4, NULL, '2023-02-20 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (3, 5, NULL, '2023-02-25 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (3, 6, NULL, '2023-02-27 21:00:00');
+
+-- TIME 4 CASA
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (4, 1, NULL, '2023-03-01 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (4, 2, NULL, '2023-03-05 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (4, 3, NULL, '2023-03-10 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (4, 5, NULL, '2023-03-15 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (4, 6, NULL, '2023-03-20 21:00:00');
+
+-- TIME 5 CASA
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (5, 1, NULL, '2023-03-25 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (5, 2, NULL, '2023-04-01 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (5, 3, NULL, '2023-04-05 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (5, 4, NULL, '2023-04-10 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (5, 6, NULL, '2023-04-15 21:00:00');
+
+-- TIME 6 CASA
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (6, 1, NULL, '2023-04-20 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (6, 2, NULL, '2023-04-25 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (6, 3, NULL, '2023-04-30 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (6, 4, NULL, '2023-05-05 21:00:00');
+
+INSERT INTO partida (id_time_casa, id_time_visitante, resultado, data_hora_partida)
+VALUES (6, 5, NULL, '2023-05-10 21:00:00');
