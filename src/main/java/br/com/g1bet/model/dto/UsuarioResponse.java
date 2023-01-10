2e5062e40043d562c50fb4a7a4f5cef84c2d2a05
Gabriel Marcolino <gabrielmr356@hotmail.com>
2023-01-10 15:17:13 -0300

    Adicinando as anotações do lombok

--- src/main/java/br/com/g1bet/model/dto/UsuarioResponse.java
+++ src/main/java/br/com/g1bet/model/dto/UsuarioResponse.java
@@ -2,9 +2,15 @@ package br.com.g1bet.model.dto;
 
 import br.com.g1bet.model.Usuario;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioResponse {
 
     private Long id;
@@ -13,18 +19,6 @@ public class UsuarioResponse {
     private LocalDate dataDeNascimento;
     private String email;
     private Double saldoUsuario;
-
-    public UsuarioResponse(Long id, String nome, LocalDate dataDeNascimento, String email, Double saldoUsuario) {
-        this.id = id;
-        this.nome = nome;
-        this.dataDeNascimento = dataDeNascimento;
-        this.email = email;
-        this.saldoUsuario = saldoUsuario;
-    }
-
-    public UsuarioResponse() {
-
-    }
 
     public static UsuarioResponse toUsuarioResponse(Usuario usuario) {
         return new UsuarioResponse(
@@ -36,43 +30,4 @@ public class UsuarioResponse {
         );
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public LocalDate getDataDeNascimento() {
-        return dataDeNascimento;
-    }
-
-    public void setDataDeNascimento(LocalDate dataDeNascimento) {
-        this.dataDeNascimento = dataDeNascimento;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Double getSaldoUsuario() {
-        return saldoUsuario;
-    }
-
-    public void setSaldoUsuario(Double saldoUsuario) {
-        this.saldoUsuario = saldoUsuario;
-    }
 }
