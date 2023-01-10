833281526d076541690bbf394371874a125eaac1
Gabriel Marcolino <gabrielmr356@hotmail.com>
2023-01-10 13:09:22 -0300

    Removendo os gets e sets e adicionando anotacao do lombok

--- src/main/java/br/com/g1bet/model/dto/UsuarioRequest.java
+++ src/main/java/br/com/g1bet/model/dto/UsuarioRequest.java
@@ -1,9 +1,11 @@
 package br.com.g1bet.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 
 import java.time.LocalDate;
 
+@Data
 public class UsuarioRequest {
 
     private String nome;
@@ -14,61 +16,5 @@ public class UsuarioRequest {
     private String senha;
     private String chavePix;
     private Double saldoUsuario;
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Long getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(Long cpf) {
-        this.cpf = cpf;
-    }
-
-    public LocalDate getDataDeNascimento() {
-        return dataDeNascimento;
-    }
-
-    public void setDataDeNascimento(LocalDate dataDeNascimento) {
-        this.dataDeNascimento = dataDeNascimento;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getChavePix() {
-        return chavePix;
-    }
-
-    public void setChavePix(String chavePix) {
-        this.chavePix = chavePix;
-    }
-
-    public Double getSaldoUsuario() {
-        return saldoUsuario;
-    }
-
-    public void setSaldoUsuario(Double saldoUsuario) {
-        this.saldoUsuario = saldoUsuario;
-    }
 
 }
